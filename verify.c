71859d583dc8e2d0c239a4c7b6c1444cf5ec45aa
Jan Dumon <jan@crossbar.net>
2018-01-20 19:32:09 +0100

    added copyright notices

--- verify.c
+++ verify.c
@@ -1,4 +1,6 @@
-/*  This program is free software: you can redistribute it and/or modify
+/*  Copyright (C) 2018  Jan Dumon <jan@crossbar.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
